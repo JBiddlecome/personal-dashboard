987fcb4aa607b7fc21e2e60c63d11d6a18587cdb
JBiddlecome <jake.biddlecome@gmail.com>
2025-10-17 08:40:14 -0700

    Add skeleton for Outlook Graph API integration
    Skeleton implementation for Outlook Calendar using Microsoft Graph API.

---
+++ providers/outlook_graph.py
@@ -0,0 +1,4 @@
+# providers/outlook_graph.py
+# Skeleton implementation for Outlook + Calendar using Microsoft Graph API
+# OAuth flow to be implemented later
+# This will provide access to Outlook emails and Microsoft Calendar events
