983b19d6f717acd69c3e00d11c7740501a3d00ce
JBiddlecome <jake.biddlecome@gmail.com>
2025-10-17 08:39:36 -0700

    Add skeleton for Gmail and Google Calendar API
    Skeleton implementation for accessing Gmail and Google Calendar.

---
+++ providers/gmail_googleapi.py
@@ -0,0 +1,4 @@
+# providers/gmail_googleapi.py
+# Skeleton implementation for Gmail + Google Calendar using Google API
+# OAuth flow to be implemented later
+# This will provide access to Gmail emails and Google Calendar events
